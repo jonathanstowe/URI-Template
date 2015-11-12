5a68966da80a6c05c7deba43673844ccf59a313e
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-11-12 09:58:50 +0000

    Basic start on the grammar

--- lib/URI/Template.pm
+++ lib/URI/Template.pm
@@ -24,9 +24,95 @@ This provides an implementation of
 L<RFC6570|https://tools.ietf.org/html/rfc6570> which allows for the
 definition of a URI through variable expansion.
 
+=head1 METHODS
+
+=head2 method new
+
+=head2 method process
+
 =end pod
 
 class URI::Template:ver<v0.0.1>:auth<github:jonathanstowe> {
+    has Str $.template;
+
+    our grammar Grammar {
+        rule TOP {
+            <expression>*
+        }
+
+        rule expression {
+            '{' <operator>? <variable>+ % ',' '}'
+
+        }
+
+        regex operator {
+            <reserved>          || 
+            <fragment>          || 
+            <label-dot>         || 
+            <path-slash>        ||
+            <path-semicolon>    ||
+            <form-ampersand>    ||
+            <form-continuation>
+
+
+        }
+
+        token reserved {
+            '+'
+        }
+        token fragment {
+            '#'
+        }
+
+        token label-dot {
+            '.'
+        }
+
+        token path-slash {
+            '/'
+        }
+
+        token path-semicolon {
+            ';'
+        }
+
+        token form-ampersand {
+            '?'
+        }
+
+        token form-continuation {
+            '&'
+        }
+
+        rule variable {
+            <variable-name><var-modifier>?
+        }
+        token variable-name {
+            <.ident>
+        }
+
+        rule var-modifier {
+            <explode> || <prefix>
+        }
+
+        token explode {
+            '*'
+        }
+        token prefix {
+            ':' <max-length>
+        }
+        token max-length {
+            \d+
+        }
+    }
+
+    my class Actions {
+
+    }
+
+    method process(*%vars) returns Str {
+
+    }
 
 }
 # vim: expandtab shiftwidth=4 ft=perl6
